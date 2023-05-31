3d63c68fc55d90abbe107d00ed4cee4fa74eb2ea
sergiy-kyiv-2022 <s22101969@gmail.com>
2023-05-31 21:29:56 +0200

    Test

---
+++ script.js
@@ -0,0 +1,7 @@
+function paint(){
+    if(document.getElementById("paragraph").style.color=="gren"){
+        document.getElementById("paragraph").style.color="black"
+    }else{
+        document.getElementById("paragraph").style.color="gren"
+    }
+}
